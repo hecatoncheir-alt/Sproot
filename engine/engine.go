9e84dd2c02293d84b3da526c93685538cd5ca82b
Vitaliy Vostrikov <rasart.pro@gmail.com>
2017-06-17 23:00:07 +0300

    Adding DatabaseSetUp method

--- engine/engine.go
+++ engine/engine.go
@@ -1,6 +1,43 @@
 package engine
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/cayleygraph/cayley/graph"
+	"github.com/google/cayley"
+)
+
+type Engine struct {
+	Store cayley.Handle
+}
+
+func New() *Engine {
+	engine := Engine{}
+	return &engine
+}
+
+func (engien *Engine) DatabaseSetUp(user, host, ssl string, baseName string) {
+	var err error
+
+	db, err := sql.Open("postgres", "postgresq"+"://"+user+"@"+host+"?"+"sslmode="+ssl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = graph.InitQuadStore("sql", "postgresql://root@192.168.99.100:26257/cayley?sslmode=disable", graph.Options{"flavor": "cockroach"})
+
+	store, err := cayley.NewGraph("sql", "postgresql://root@192.168.99.100:26257/cayley?sslmode=disable", graph.Options{"flavor": "cockroach"})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer store.Close()
+
+}
+
 // SavePriceForProductOfCompany method for save subject, predicate and object in graph database
-func SavePriceForProductOfCompany(item Item) (*PriceOfProduct, error) {
+func (engine *Engine) SavePriceForProductOfCompany(item Item) (*PriceOfProduct, error) {
 	return nil, nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/hecatoncheir/Sproot/engine"
 
+func main() {
+	puffer := engine.New()
 }
