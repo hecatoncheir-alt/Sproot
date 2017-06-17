a33c67dee0811635e59d5ca092ef353b386c6467
Vitaliy Vostrikov <rasart.pro@gmail.com>
2017-06-17 19:09:56 +0300

    Adding tests

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -4,11 +4,16 @@ import (
 	"testing"
 )
 
+// {Смартфон Samsung Galaxy S8+ 64Gb Мистический аметист http://www.mvideo.ru//products/smartfon-samsung-galaxy-s8-64gb-misticheskii-ametist-30027823 img.mvideo.ru/Pdb/30027823m.jpg {59990  2017-06-17 16:07:13.888498569 +0000 UTC} { http://www.mvideo.ru/ M.Video [Телефоны]}}
 func TestSprootCanSaveGetAndDeleteData(test *testing.T) {
+	incomingItem := Item{}
 
-	item, err := SavePriceForProductOfCompany()
-
+	item, err := SavePriceForProductOfCompany(incomingItem)
 	if err != nil {
 		test.Error(err)
+	}
+
+	if item.Name != "test item name" {
+		test.Fail()
 	}
 }
--- engine/output_entities.go
+++ engine/output_entities.go
@@ -1,5 +1,5 @@
 package engine
 
 type PriceOfProduct struct {
-
+	Name string
 }
