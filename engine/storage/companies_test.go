f005f3372e6f96eab2d7320b888fd467a087933e
Rasarts <rasart.pro@gmail.com>
2017-12-23 15:20:29 +0300

    Added DeactivateCompany method.

--- engine/storage/companies_test.go
+++ engine/storage/companies_test.go
@@ -13,9 +13,9 @@ func TestIntegrationCompanyCanBeReadByName(test *testing.T) {
 		test.Error(err)
 	}
 
-	companyForSearch := Company{Name: "Test company"}
+	companyForTest := Company{Name: "Test company"}
 
-	companiesFromStore, err := storage.Companies.ReadCompaniesByName(companyForSearch.Name)
+	companiesFromStore, err := storage.Companies.ReadCompaniesByName(companyForTest.Name)
 	if err != ErrCompaniesByNameNotFound {
 		test.Fail()
 	}
@@ -23,29 +23,75 @@ func TestIntegrationCompanyCanBeReadByName(test *testing.T) {
 	if companiesFromStore != nil {
 		test.Fail()
 	}
-	//
-	//createdCategory, err := storage.Categories.CreateCategory(&categoryForSearch)
-	//if err != nil {
-	//	test.Error(err)
-	//}
-	//
-	//defer storage.Categories.DeleteCategory(createdCategory)
-	//
-	//categoriesFromStore, err = storage.Categories.ReadCategoriesByName(createdCategory.Name)
-	//if err != nil {
-	//	test.Fail()
-	//}
-	//
-	//if categoriesFromStore[0].Name != createdCategory.Name {
-	//	test.Fail()
-	//}
-	//
-	//if categoriesFromStore[0].ID == "" {
-	//	test.Fail()
-	//}
+
+	createdCompany, err := storage.Companies.CreateCompany(&companyForTest)
+	if err != nil || createdCompany.ID == "" {
+		test.Fail()
+	}
+
+	defer storage.Companies.DeactivateCompany(createdCompany)
+
+	companiesFromStore, err = storage.Companies.ReadCompaniesByName(createdCompany.Name)
+	if err != nil {
+		test.Fail()
+	}
+
+	if companiesFromStore[0].Name != createdCompany.Name {
+		test.Fail()
+	}
+
+	if companiesFromStore[0].ID == "" {
+		test.Fail()
+	}
 }
 
-func TestIntegrationCompanyCanBeDeleted(test *testing.T) {
+func TestIntegrationCompanyCanBeReadById(test *testing.T) {
+	var err error
+	storage := New(databaseHost, databasePort)
+
+	err = storage.SetUp()
+	if err != nil {
+		test.Error(err)
+	}
+
+	companyForSearch := Company{Name: "Test category"}
+
+	companiesFromStore, err := storage.Companies.ReadCompaniesByName(companyForSearch.Name)
+	if err != ErrCompaniesByNameNotFound {
+		test.Fail()
+	}
+
+	companyFromStore, err := storage.Companies.ReadCompanyByID("0")
+	if err != ErrCompanyDoesNotExist {
+		test.Fail()
+	}
+
+	if companiesFromStore != nil {
+		test.Fail()
+	}
+
+	createdCompany, err := storage.Companies.CreateCompany(&companyForSearch)
+	if err != nil {
+		test.Error(err)
+	}
+
+	defer storage.Companies.DeactivateCompany(createdCompany)
+
+	companyFromStore, err = storage.Companies.ReadCompanyByID(createdCompany.ID)
+	if err != nil {
+		test.Fail()
+	}
+
+	if companyFromStore.Name != createdCompany.Name {
+		test.Fail()
+	}
+
+	if companyFromStore.ID == "" {
+		test.Fail()
+	}
+}
+
+func TestIntegrationCompanyCanBeDeactivate(test *testing.T) {
 	var err error
 	storage := New(databaseHost, databasePort)
 
@@ -55,12 +101,12 @@ func TestIntegrationCompanyCanBeDeleted(test *testing.T) {
 	}
 
 	companyForTest := Company{Name: "Test company"}
-	//createdCategory, err := storage.Companies.CreateCategory(&companyForTest)
-	//if err != nil {
-	//	test.Error(err)
-	//}
+	createdCompany, err := storage.Companies.CreateCompany(&companyForTest)
+	if err != nil {
+		test.Error(err)
+	}
 
-	deletedCompanyID, err := storage.Companies.DeleteCompany(companyForTest)
+	deletedCompanyID, err := storage.Companies.DeactivateCompany(createdCompany)
 	if err != nil {
 		if err != ErrCompanyCanNotBeWithoutID {
 			test.Error(err)
@@ -71,8 +117,8 @@ func TestIntegrationCompanyCanBeDeleted(test *testing.T) {
 		test.Fail()
 	}
 
-	//_, err = storage.Categories.ReadCategoryByID(deletedCategoryID)
-	//if err != ErrCategoryDoesNotExist {
-	//	test.Error(err)
-	//}
+	_, err = storage.Companies.ReadCompanyByID(deletedCompanyID)
+	if err != ErrCompanyDoesNotExist {
+		test.Error(err)
+	}
 }
