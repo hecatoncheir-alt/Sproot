60bdeb6d982da7d71ac577393f2152396349376b
Rasarts <rasart.pro@gmail.com>
2018-01-16 22:31:06 +0300

    Adding prices resource methods.

--- engine/storage/prices.go
+++ engine/storage/prices.go
@@ -2,6 +2,9 @@ package storage
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	dataBaseAPI "github.com/dgraph-io/dgraph/protos/api"
 	"log"
 	"time"
@@ -45,4 +48,117 @@ func (prices *Prices) SetUp() (err error) {
 	}
 
 	return nil
+}
+
+// ErrPriceCanNotBeCreated means that the price can't be added to database
+var ErrPriceCanNotBeCreated = errors.New("price can't be created")
+
+// CreatePrice is a method for make product and save it to storage
+func (prices *Prices) CreatePrice(price Price) (Price, error) {
+	transaction := prices.storage.Client.NewTxn()
+
+	price.IsActive = true
+	encodedPrice, err := json.Marshal(price)
+	if err != nil {
+		log.Println(err)
+		return price, ErrPriceCanNotBeCreated
+	}
+
+	mutation := &dataBaseAPI.Mutation{
+		SetJson:   encodedPrice,
+		CommitNow: true}
+
+	assigned, err := transaction.Mutate(context.Background(), mutation)
+	if err != nil {
+		log.Println(err)
+		return price, ErrPriceCanNotBeCreated
+	}
+
+	price.ID = assigned.Uids["blank-0"]
+	if price.ID == "" {
+		return price, ErrPriceCanNotBeCreated
+	}
+
+	return price, nil
+}
+
+// ErrPriceCanNotBeDeleted means that the price can't be deleted from database
+var ErrPriceCanNotBeDeleted = errors.New("price can't be deleted")
+
+// DeletePrice method for remove price from database
+func (prices *Prices) DeletePrice(price Price) (string, error) {
+	var err error
+
+	deletePriceData, err := json.Marshal(map[string]string{"uid": price.ID})
+	if err != nil {
+		log.Println(err)
+		return price.ID, ErrPriceCanNotBeDeleted
+	}
+
+	mutation := dataBaseAPI.Mutation{
+		DeleteJson: deletePriceData,
+		CommitNow:  true}
+
+	transaction := prices.storage.Client.NewTxn()
+
+	_, err = transaction.Mutate(context.Background(), &mutation)
+	if err != nil {
+		log.Println(err)
+		return price.ID, ErrPriceCanNotBeDeleted
+	}
+
+	return price.ID, nil
+}
+
+// ErrPriceByIDCanNotBeFound means that the price can't be found in database
+var ErrPriceByIDCanNotBeFound = errors.New("price by id can not be found")
+
+// ErrPriceDoesNotExist means than the price does not exist in database
+var ErrPriceDoesNotExist = errors.New("price by id not found")
+
+// ReadPriceByID is a method for get all nodes of prices by ID
+func (prices *Prices) ReadPriceByID(priceID, language string) (Price, error) {
+	price := Price{ID: priceID}
+
+	query := fmt.Sprintf(`{
+				prices(func: uid("%s")) {
+					uid
+					priceValue
+					priceDateTime
+					priceCity
+					priceIsActive
+					belongs_to_product @filter(eq(productIsActive, true)) {
+						uid
+						productName: productName@%v
+						productIri
+						previewImageLink
+						productIsActive
+					}
+				}
+			}`, priceID, language)
+
+	transaction := prices.storage.Client.NewTxn()
+	response, err := transaction.Query(context.Background(), query)
+	if err != nil {
+		log.Println(err)
+		return price, ErrPriceByIDCanNotBeFound
+	}
+
+	type PricesInStore struct {
+		Prices []Price `json:"prices"`
+	}
+
+	var foundedPrices PricesInStore
+
+	err = json.Unmarshal(response.GetJson(), &foundedPrices)
+	if err != nil {
+		log.Println(err)
+		return price, ErrPriceByIDCanNotBeFound
+	}
+
+	if len(foundedPrices.Prices) == 0 {
+		return price, ErrPriceDoesNotExist
+	}
+
+	return foundedPrices.Prices[0], nil
 }
--- engine/storage/prices_test.go
+++ engine/storage/prices_test.go
@@ -1,1 +1,94 @@
 package storage
+
+import (
+	"testing"
+	"time"
+)
+
+func TestIntegrationPriceCanBeCreated(test *testing.T) {
+	once.Do(prepareStorage)
+
+	exampleDateTime := "2017-05-01T16:27:18.543653798Z"
+	dateTime, err := time.Parse(time.RFC3339, exampleDateTime)
+	if err != nil {
+		test.Error(err)
+	}
+
+	priceForCreate := Price{Value: 21.440, DateTime: dateTime}
+	createdPrice, err := storage.Prices.CreatePrice(priceForCreate)
+	if err != nil {
+		test.Error(err)
+	}
+
+	defer storage.Prices.DeletePrice(createdPrice)
+
+	if createdPrice.ID == "" {
+		test.Fail()
+	}
+}
+
+func TestIntegrationPriceCanBeReadById(test *testing.T) {
+	once.Do(prepareStorage)
+
+	exampleDateTime := "2017-05-01T16:27:18.543653798Z"
+	dateTime, err := time.Parse(time.RFC3339, exampleDateTime)
+	if err != nil {
+		test.Error(err)
+	}
+
+	priceForCreate := Price{Value: 21.440, DateTime: dateTime}
+	createdPrice, err := storage.Prices.CreatePrice(priceForCreate)
+
+	defer storage.Prices.DeletePrice(createdPrice)
+
+	priceFromStore, err := storage.Prices.ReadPriceByID(createdPrice.ID, ".")
+	if err != nil {
+		test.Fail()
+	}
+
+	if priceFromStore.ID != createdPrice.ID {
+		test.Fail()
+	}
+
+	if priceFromStore.IsActive == false {
+		test.Fail()
+	}
+
+	if priceFromStore.ID == "" {
+		test.Fail()
+	}
+}
+
+func TestIntegrationPriceCanBeDeleted(test *testing.T) {
+	test.Skip()
+	once.Do(prepareStorage)
+	storage.DeleteAll()
+	storage.SetUp()
+
+	exampleDateTime := "2017-05-01T16:27:18.543653798Z"
+	dateTime, err := time.Parse(time.RFC3339, exampleDateTime)
+	if err != nil {
+		test.Error(err)
+	}
+
+	priceForCreate := Price{Value: 22.440, DateTime: dateTime}
+
+	createdPrice, err := storage.Prices.CreatePrice(priceForCreate)
+	if err != nil {
+		test.Error(err)
+	}
+
+	deletedPriceID, err := storage.Prices.DeletePrice(createdPrice)
+	if err != nil {
+		test.Error(err)
+	}
+
+	if deletedPriceID != createdPrice.ID {
+		test.Fail()
+	}
+
+	_, err = storage.Prices.ReadPriceByID(deletedPriceID, ".")
+	if err != ErrPriceDoesNotExist {
+		test.Error(err)
+	}
+}
