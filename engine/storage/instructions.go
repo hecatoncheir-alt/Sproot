2aa8bad013e9d95cbf7bae630b80581d4a51cca9
Rasarts <rasart.pro@gmail.com>
2018-01-26 23:52:48 +0300

    Adding PageInstruction tests.

--- engine/storage/instructions.go
+++ engine/storage/instructions.go
@@ -2,6 +2,9 @@ package storage
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	dataBaseAPI "github.com/dgraph-io/dgraph/protos/api"
 	"log"
 )
@@ -20,11 +23,9 @@ type PageInstruction struct {
 
 // Instruction is a structure of instruction for parse
 type Instruction struct {
-	ID       string `json:"uid, omitempty"`
-	Language string `json:"instructionLanguage, omitempty"`
-
-	IsActive bool `json:"instructionIsActive, omitempty"`
-
+	ID        string            `json:"uid, omitempty"`
+	Language  string            `json:"instructionLanguage, omitempty"`
+	IsActive  bool              `json:"instructionIsActive, omitempty"`
 	Pages     []PageInstruction `json:"has_pages, omitempty"`
 	Cities    []City            `json:"has_city, omitempty"`
 	Companies []Company         `json:"has_company, omitempty"`
@@ -67,4 +68,96 @@ func (instructions *Instructions) SetUp() (err error) {
 	}
 
 	return nil
+}
+
+func (instructions *Instructions) CreatePageInstruction(pageInstruction PageInstruction) (PageInstruction, error) {
+	transaction := instructions.storage.Client.NewTxn()
+
+	encodedPageInstruction, err := json.Marshal(pageInstruction)
+	if err != nil {
+		log.Println(err)
+		return pageInstruction, err
+	}
+
+	mutation := &dataBaseAPI.Mutation{
+		SetJson:   encodedPageInstruction,
+		CommitNow: true}
+
+	assigned, err := transaction.Mutate(context.Background(), mutation)
+	if err != nil {
+		log.Println(err)
+		return pageInstruction, err
+	}
+
+	pageInstruction.ID = assigned.Uids["blank-0"]
+
+	return pageInstruction, nil
+}
+
+// ErrPageInstructionDoesNotExist means than the page instruction does not exist in database
+var ErrPageInstructionDoesNotExist = errors.New("page instruction by id not found")
+
+func (instructions *Instructions) ReadPageInstructionByID(pageInstructionID string) (PageInstruction, error) {
+	pageInstruction := PageInstruction{ID: pageInstructionID}
+
+	query := fmt.Sprintf(`{
+				pageInstructions(func: uid("%s")) @filter(has(path)) {
+					uid
+					path
+					pageInPaginationSelector
+					pageParamPath
+					cityParamPath
+					cityParam
+					itemSelector
+					nameOfItemSelector
+					priceOfItemSelector
+				}
+			}`, pageInstructionID)
+
+	transaction := instructions.storage.Client.NewTxn()
+	response, err := transaction.Query(context.Background(), query)
+	if err != nil {
+		log.Println(err)
+		return pageInstruction, err
+	}
+
+	type PageInstructionsInStorage struct {
+		PageInstructions []PageInstruction `json:"pageInstructions"`
+	}
+
+	var foundedPageInstructions PageInstructionsInStorage
+
+	err = json.Unmarshal(response.GetJson(), &foundedPageInstructions)
+	if err != nil {
+		log.Println(err)
+		return pageInstruction, err
+	}
+
+	if len(foundedPageInstructions.PageInstructions) == 0 {
+		return pageInstruction, ErrPageInstructionDoesNotExist
+	}
+
+	return foundedPageInstructions.PageInstructions[0], nil
+}
+
+func (instructions *Instructions) DeletePageInstruction(pageInstruction PageInstruction) (string, error) {
+	deletePageInstructionData, err := json.Marshal(map[string]string{"uid": pageInstruction.ID})
+	if err != nil {
+		log.Println(err)
+		return pageInstruction.ID, err
+	}
+
+	mutation := dataBaseAPI.Mutation{
+		DeleteJson: deletePageInstructionData,
+		CommitNow:  true}
+
+	transaction := instructions.storage.Client.NewTxn()
+
+	_, err = transaction.Mutate(context.Background(), &mutation)
+	if err != nil {
+		log.Println(err)
+		return pageInstruction.ID, err
+	}
+
+	return pageInstruction.ID, nil
 }
--- engine/storage/instructions_test.go
+++ engine/storage/instructions_test.go
@@ -4,6 +4,89 @@ import (
 	"testing"
 )
 
-func TestIntegrationInstructionCanBeCreated(test *testing.T) {
+func TestIntegrationPageInstructionCanBeCreated(test *testing.T) {
 	once.Do(prepareStorage)
+
+	mVideoPageInstruction := PageInstruction{
+		Path: "smartfony-i-svyaz/smartfony-205",
+		PageInPaginationSelector: ".pagination-list .pagination-item",
+		PageParamPath:            "/f/page=",
+		CityParamPath:            "?cityId=",
+		CityParam:                "CityCZ_975",
+		ItemSelector:             ".grid-view .product-tile",
+		NameOfItemSelector:       ".product-tile-title",
+		PriceOfItemSelector:      ".product-price-current"}
+
+	createdPageInstruction, err := storage.Instructions.CreatePageInstruction(mVideoPageInstruction)
+	if err != nil {
+		test.Fail()
+	}
+
+	defer storage.Instructions.DeletePageInstruction(createdPageInstruction)
+
+	if createdPageInstruction.ID == "" {
+		test.Fail()
+	}
+}
+
+func TestIntegrationPageInstructionCanBeReadById(test *testing.T) {
+	once.Do(prepareStorage)
+	mVideoPageInstruction := PageInstruction{
+		Path: "smartfony-i-svyaz/smartfony-205",
+		PageInPaginationSelector: ".pagination-list .pagination-item",
+		PageParamPath:            "/f/page=",
+		CityParamPath:            "?cityId=",
+		CityParam:                "CityCZ_975",
+		ItemSelector:             ".grid-view .product-tile",
+		NameOfItemSelector:       ".product-tile-title",
+		PriceOfItemSelector:      ".product-price-current"}
+
+	createdPageInstruction, err := storage.Instructions.CreatePageInstruction(mVideoPageInstruction)
+	if err != nil {
+		test.Fail()
+	}
+
+	defer storage.Instructions.DeletePageInstruction(createdPageInstruction)
+
+	pageInstructionFromStore, err := storage.Instructions.ReadPageInstructionByID(createdPageInstruction.ID)
+	if err != nil {
+		test.Fail()
+	}
+
+	if createdPageInstruction.ID != pageInstructionFromStore.ID {
+		test.Fail()
+	}
+}
+
+func TestIntegrationPageInstructionCanBeDeleted(test *testing.T) {
+	once.Do(prepareStorage)
+
+	mVideoPageInstruction := PageInstruction{
+		Path: "smartfony-i-svyaz/smartfony-205",
+		PageInPaginationSelector: ".pagination-list .pagination-item",
+		PageParamPath:            "/f/page=",
+		CityParamPath:            "?cityId=",
+		CityParam:                "CityCZ_975",
+		ItemSelector:             ".grid-view .product-tile",
+		NameOfItemSelector:       ".product-tile-title",
+		PriceOfItemSelector:      ".product-price-current"}
+
+	createdPageInstruction, err := storage.Instructions.CreatePageInstruction(mVideoPageInstruction)
+	if err != nil {
+		test.Fail()
+	}
+
+	deletedPageInstructionID, err := storage.Instructions.DeletePageInstruction(createdPageInstruction)
+	if err != nil {
+		test.Error(err)
+	}
+
+	if deletedPageInstructionID != deletedPageInstructionID {
+		test.Fail()
+	}
+
+	_, err = storage.Instructions.ReadPageInstructionByID(createdPageInstruction.ID)
+	if err != ErrPageInstructionDoesNotExist {
+		test.Error(err)
+	}
 }
